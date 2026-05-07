d7d1367bb408a49aca7d9acf5270572f2cee31b5
256-kb <nino.chevalier@outlook.fr>
2026-05-07 23:28:07 +0200

    Update and rename supabase.js to to delete

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  "https://xrsnqgztqetglmsnjhhd.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhyc25xZ3p0cWV0Z2xtc25qaGhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNzMzNTYsImV4cCI6MjA5Mzc0OTM1Nn0.6xQu3deSDEmvfCQQc5m4zf81tobR-C7iRA07XRKndJc"
-);
