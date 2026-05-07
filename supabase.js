7a81bf788731ed42b8199c5104e318a2ae089133
256-kb <nino.chevalier@outlook.fr>
2026-05-07 21:21:31 +0200

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://xrsnqgztqetglmsnjhhd.supabase.co",
+  "sb_publishable_48qp4xHYQ_-VkjX90BT6_Q_yTFJs81r"
+);
